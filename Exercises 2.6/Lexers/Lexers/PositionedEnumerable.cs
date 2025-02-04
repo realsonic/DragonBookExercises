90a4967efe79c98272e354d2905bcf20b68aa5d9
m.balanov <m.balanov@samolet.ru>
2025-02-05 01:27:24 +0300

    added PositionedEnumerable

---
+++ Exercises 2.6/Lexers/Lexers/PositionedEnumerable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+
+namespace Lexers;
+
+public class PositionedEnumerable(IEnumerable<char> enumerable) : IEnumerable<(char, Position)>
+{
+    IEnumerator<(char, Position)> IEnumerable<(char, Position)>.GetEnumerator()
+    {
+        return Enumerate().GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return Enumerate().GetEnumerator();
+    }
+
+    private IEnumerable<(char, Position)> Enumerate()
+    {
+        Position position = new(1, 0);
+
+        foreach (char character in enumerable)
+        {
+            position = character is '\n' ? position.NewLine() : position.AddColumn();
+            yield return (character, position);
+        }
+    }
+}
